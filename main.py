e7bbe6bf63e6efa38f8666dde32c99ddc571de49
Team-77 <82922812+Teams77@users.noreply.github.com>
2023-03-19 14:49:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,30 +11,30 @@ discord = Discord(url=webhook) ##Enter Webhook Here
 error = 0
 while True:
     user = ""
-    for character in random.choices("abcdefghijklmnopqrstuvwxyz1234567890", k=2):
+    for character in random.choices("abcdefghijklmnopqrstuvwxyz1234567890", k=2): ##By Team 77 
         user = user + character
-    response = requests.get(f"https://discord.com/api/v9/invites/{user}?with_counts=true&with_expiration=true")
-    print(Fore.RESET + Fore.GREEN + f"Available: {user}")
-    data =(user)
-    discord.post(content=F"**Free Vanity : `{user}` **")
+    response = requests.get(f"https://discord.com/api/v9/invites/{user}?with_counts=true&with_expiration=true")##By Team 77 
+    print(Fore.RESET + Fore.GREEN + f"Available: {user}")##By Team 77 
+    data =(user)##By Team 77 
+    discord.post(content=F"**Free Vanity : `{user}` **")##By Team 77 
     time.sleep(0.00005)
     
-    users = ""
-    for characters in random.choices("1234567890qwertyuiopasdfghjklzxcvbnm", k=3):
+    users = ""##By Team 77 
+    for characters in random.choices("1234567890qwertyuiopasdfghjklzxcvbnm", k=3):##By Team 77 
+        users = users + characters##By Team 77 
+    response = requests.get(f"https://discord.com/api/v9/invites/{users}?with_counts=true&with_expiration=true")##By Team 77 
+    print(Fore.RESET + Fore.GREEN + f"Available: {users}")##By Team 77 
+    data =(users)##By Team 77 
+    discord.post(content=F"**Free Vanity : `{users}` **")##By Team 77 
+    time.sleep(0.00005)##By Team 77 
+    ##By Team 77 
+##By Team 77 
+    users = ""##By Team 77 
+    for characters in random.choices("1234567890qwertyuiopasdfghjklzxcvbnm", k=4):##By Team 77 
         users = users + characters
-    response = requests.get(f"https://discord.com/api/v9/invites/{users}?with_counts=true&with_expiration=true")
-    print(Fore.RESET + Fore.GREEN + f"Available: {users}")
-    data =(users)
-    discord.post(content=F"**Free Vanity : `{users}` **")
-    time.sleep(0.00005)
-    
-
-    users = ""
-    for characters in random.choices("1234567890qwertyuiopasdfghjklzxcvbnm", k=4):
-        users = users + characters
-    response = requests.get(f"https://discord.com/api/v9/invites/{users}?with_counts=true&with_expiration=true")
-    print(Fore.RESET + Fore.GREEN + f"Available: {users}")
-    data =(users)
-    discord.post(content=F"**Free Vanity : `{users}` **")
-    time.sleep(0.00005)
+    response = requests.get(f"https://discord.com/api/v9/invites/{users}?with_counts=true&with_expiration=true")##By Team 77 
+    print(Fore.RESET + Fore.GREEN + f"Available: {users}")##By Team 77 
+    data =(users)##By Team 77 
+    discord.post(content=F"**Free Vanity : `{users}` **")##By Team 77 
+    time.sleep(0.00005)##By Team 77 
     
